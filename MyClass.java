0b053c732053dc40f96e0db7fe349b589a196222
Jarvismahesh <109132867+Jarvismahesh@users.noreply.github.com>
2023-08-21 17:05:20 +0530

    Request created by mahesh

--- MyClass.java
+++ MyClass.java
@@ -1,3 +1,14 @@
 class MyClass {
-
+public int mahesh_method_add()
+{
+    int val1,int val2;
+    int Add_result = val1+val2;
+    return Add_result;
+}
+public int mahesh_method_subract()
+{
+    int val3,val4;
+    int Sub_result = val3+val4;
+    return Sub_result;
+}
 }
